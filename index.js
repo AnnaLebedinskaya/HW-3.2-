05c3fc80111cbfe84178bd3b55d7204337a90da9
Anna <anna.vasiljeva94@gmail.com>
2024-07-15 15:50:11 +0300

    new index.js

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const age = 42;
-console.log("age:", typeof age);
-const text = "Hello, world!";
-console.log("text:", typeof text);
-const permission = true;
-console.log("permission:", typeof permission);
-const emptyValue = null;
-console.log("emptyValue:", typeof emptyValue); 
-const variable = undefined;
-console.log("variable:", typeof variable);
-const symbol = Symbol("symbol");
-console.log("symbol:", typeof symbol);
-const cup = { color: "red" };
-console.log("cup:", typeof cup);
-const array = [1, 2, 3];
-console.log("array:", typeof array); 
-const func = function() { return "I am a function"; };
-console.log("function:", typeof func);
-const theBiggestInt = BigInt(9007199254740991);
-console.log("theBiggestInt:", typeof theBiggestInt);
+const surname = `Васильєва`;
+const firstName = `Ганна`;
+const patronymic = `Володимирівна`;
+const greeting = `Привіт, ${surname} ${firstName} ${patronymic}!`;
+console.log(greeting);
+
+
 
 
 
